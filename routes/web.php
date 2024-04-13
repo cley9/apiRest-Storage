641d3844bccc18adc30d11d215691d00a0ba7b47
cley9 <cleyutp@gmail.com>
2024-04-13 18:30:05 -0500

    v. 1.2.0 added storage more enmascaramiento of image Api

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\api\StorageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "ApiRest Storage"
 });
 Route::post('/api/v1/storage', [StorageController::class, 'storage'])->name('storage.api');
 Route::get('/api/v1/image/{token}', [StorageController::class, 'enmascarar'])->name('image.api.enmascarar');
